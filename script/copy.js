bb3494aaaef11fd44c4916dbe71014f4cb41b891
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-09-01 21:04:49 +0530

    copy feature added

--- script/copy.js
+++ script/copy.js
@@ -1,10 +1,17 @@
-{/* <p id="text">Hello</p>
-<button onclick="copyToClipboard('#text')"></button> */}
 
-function copyToClipboard(element) {
-  var temp = ("<input>");
-  ("body").append(temp);
-  temp.val((element).text()).select();
-  document.execCommand("copy");
-  temp.remove();
+{/* <p id="sample">Hello World</p>
+
+The following link copies the text within the specified element (in this sample the paragraph with id="sample") to the clipboard:
+
+<a href="#" onclick="CopyToClipboard('sample');return false;">Copy Text</a> */}
+
+
+function CopyToClipboard(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
 }
